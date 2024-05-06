aa581a807e44fc4bd55a50fc74b99b92e96acaa5
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2024-05-06 10:13:21 -0400

    Page Template: cleanup & make full width.
    - Remove post_thumbnail as its not used on pages.
    - Remove comments_template as its not used on pages.
    - Remove get_sidebar as don't like on pages pages.

--- page.php
+++ page.php
@@ -21,27 +21,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 
 if ( have_posts()) : while (have_posts() ) : the_post(); ?>
-
-	<?php
-	// Show featured image
-	if ( has_post_thumbnail( get_the_ID() ) ) : ?>
-		<div id="page-featured-img"><?php the_post_thumbnail( 'full' ); ?></div>
-	<?php endif; ?>
-
-	<div id="page-heading"><h1><?php the_title(); ?></h1></div><!-- /page-heading -->
-
-	<div id="single-page-content" class="container sidebar-bg clearfix">
-
-		<article id="post" class="clearfix">
-			<div class="entry clearfix">	
-				<?php the_content(); ?>
-			</div><!-- .entry -->         
-			<?php comments_template(); ?>
-		</article><!-- #post -->
-
-		<?php get_sidebar();?>
-
-	</div><!-- #single-page-content -->
+    <div id="single-page-content" class="container clearfix">
+        <div class="entry clearfix">
+            <?php the_content(); ?>
+        </div><!-- .entry -->
+    </div><!-- #single-page-content -->
 
 <?php endwhile; endif; ?>
 
