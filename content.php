41e741e2e89062ce62bcecb50e3daf33fb7a80d6
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2024-05-10 16:36:14 -0400

    Some Micro fixes.
    - Make post post_thumbnail clickable.
    - Remove is is_singular as content.php is no longer use for post formates.

--- content.php
+++ content.php
@@ -13,38 +13,30 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	return;
 }
-
-if ( is_singular() ) { ?>
-
-	<div id="post-thumbnail"><?php the_post_thumbnail( 'wpex-post' ); ?></div>
-
-<?php } else { ?>
-
-	<article <?php post_class('blog-entry clearfix'); ?>>
-            <?php if (universal_is_displaying_media_gl()) : ?>
-                <?php get_template_part('formats/format', get_post_format()); ?>
-            <?php else: ?>
-                <?php get_template_part('formats/format'); ?>
-            <?php endif; ?>
-            <div class="entry-text clearfix">
-                <header class="u-wrap-text">
-                    <h2>
-                    <?php if (universal_is_displaying_media_gl()) : ?>
-                        <?php the_title(); ?>
-                    <?php else: ?>
-                        <a href="<?php the_permalink(); ?>" title="<?php wpex_esc_title(); ?>"><?php the_title(); ?></a>
-                    <?php endif; ?>
-                    </h2>
-                </header>
-            <div class="u-wrap-text u-trim" style="--u-line-clamp: 6">
-                <?php wpex_excerpt(); ?>
-            </div>
-			<ul class="entry-meta">
-				<li><strong>Posted on:</strong> <?php echo get_the_date(); ?></li>
-				<li><strong>By:</strong> <?php the_author_posts_link(); ?></li>   
-				<?php if(comments_open()) { ?><li class="comment-scroll"><strong>With:</strong> <?php comments_popup_link(__('0 Comments', 'tetris'), __('1 Comment', 'tetris'), __('% Comments', 'tetris'), 'comments-link' ); ?></li><?php } ?>
-			</ul><!-- /entry-meta -->
-		</div><!-- /entry-text -->
-	</article><!-- /blog-entry -->
-
-<?php } ?>
+?> 
+<article <?php post_class('blog-entry clearfix'); ?>>
+        <?php if (universal_is_displaying_media_gl()) : ?>
+            <?php get_template_part('formats/format', get_post_format()); ?>
+        <?php else: ?>
+            <?php get_template_part('formats/format'); ?>
+        <?php endif; ?>
+        <div class="entry-text clearfix">
+            <header class="u-wrap-text">
+                <h2>
+                <?php if (universal_is_displaying_media_gl()) : ?>
+                    <?php the_title(); ?>
+                <?php else: ?>
+                    <a href="<?php the_permalink(); ?>" title="<?php wpex_esc_title(); ?>"><?php the_title(); ?></a>
+                <?php endif; ?>
+                </h2>
+            </header>
+        <div class="u-wrap-text u-trim" style="--u-line-clamp: 6">
+            <?php wpex_excerpt(); ?>
+        </div>
+        <ul class="entry-meta">
+            <li><strong>Posted on:</strong> <?php echo get_the_date(); ?></li>
+            <li><strong>By:</strong> <?php the_author_posts_link(); ?></li>
+            <?php if(comments_open()) { ?><li class="comment-scroll"><strong>With:</strong> <?php comments_popup_link(__('0 Comments', 'tetris'), __('1 Comment', 'tetris'), __('% Comments', 'tetris'), 'comments-link' ); ?></li><?php } ?>
+        </ul><!-- /entry-meta -->
+    </div><!-- /entry-text -->
+</article><!-- /blog-entry -->
