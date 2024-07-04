ae221ff15dc232e618404b53929723ff3a4b60c8
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2024-07-03 22:28:24 -0400

    Give universal meta box even better.
    - Rename generate_media_row to add_media_library_control.
    - Move type="hidden" to add_media_library_control & make it dynamic.
    - Move get_post_meta to add_media_library_control & make it dynamic.
    - Add save_media_library_selection function.
    - Add clear button next to each upload media button.
    - Wrap all button in the row in a div with class universal_group.
    - Make button-link-delete border match icon color.
    - Remove colon symbol form all label.

--- js/universal-meta-box.js
+++ js/universal-meta-box.js
@@ -1,4 +1,19 @@
 jQuery(document).ready(function($) {
+    // Hide clear all buttons initially if there are no media items selected
+    function initializeClearButtons() {
+        if ($('#universal_local_audio_attachment_ids').val()) {
+            $('#universal_local_media_clear_all_audio').fadeIn();
+        }
+        if ($('#universal_local_video_attachment_ids').val()) {
+            $('#universal_local_media_clear_all_video').fadeIn();
+        }
+        if ($('#universal_local_image_attachment_ids').val()) {
+            $('#universal_local_media_clear_all_image').fadeIn();
+        }
+    }
+
+    initializeClearButtons();
+
     // Function to open the media uploader and handle selection
     function openMediaUploader(mediaType, inputFieldId) {
         var mediaUploader = wp.media({
@@ -8,19 +23,18 @@ jQuery(document).ready(function($) {
             states: [
                 new wp.media.controller.Library({
                     title: 'Upload ' + mediaType.charAt(0).toUpperCase() + mediaType.slice(1),
-                    library: wp.media.query({
-                        type: mediaType
-                    }),
-                    date: false,
-                    priority: 20,
+                    library: wp.media.query({ type: mediaType }),
+                    date: true,
+                    editable: true,
                     sortable: true,
                     multiple: 'add',
                     searchable: true,
                     autoSelect: true,
-                    filterable: false,
                     syncSelection: true,
+                    filterable: false,
                     allowLocalEdits: true,
                     displaySettings: false,
+                    contentUserSetting: true,
                     displayUserSettings: true,
                 }),
             ]
@@ -56,17 +70,45 @@ jQuery(document).ready(function($) {
 
             // Store selected IDs in local storage
             localStorage.setItem('selected_' + mediaType + '_ids', attachmentIds.join(','));
+            
+            // Show the clear button
+            $('#universal_local_media_clear_all_' + mediaType).fadeIn();
+        });
+
+        // Handle the event when media items are selected or reordered
+        mediaUploader.on('display', function() {
+            var attachments = mediaUploader.state().get('selection').toJSON();
+            var attachmentIds = [];
+
+            // Extract IDs of selected attachments
+            attachments.forEach(function(attachment) {
+                attachmentIds.push(attachment.id);
+            });
+
+            // Store selected IDs in the input field
+            $(inputFieldId).val(attachmentIds.join(','));
+
+            // Store selected IDs in local storage
+            localStorage.setItem('selected_' + mediaType + '_ids', attachmentIds.join(','));
+            
+            // Show the clear button
+            $('#universal_local_media_clear_all_' + mediaType).fadeIn();
         });
 
         mediaUploader.open();
     }
 
+    function clearAllMedia(mediaType) {
+        $('#universal_local_' + mediaType + '_attachment_ids').val('');
+        localStorage.removeItem('selected_' + mediaType + '_ids');
+    }
+
+    // Handle click events for each media upload button
     $('#universal_local_media_upload_video').click(function(e) {
         e.preventDefault();
         openMediaUploader('video', '#universal_local_video_attachment_ids');
     });
 
-    // Handle click events for each media upload button with hard-coded mediaType
     $('#universal_local_media_upload_audio').click(function(e) {
         e.preventDefault();
         openMediaUploader('audio', '#universal_local_audio_attachment_ids');
@@ -75,5 +117,24 @@ jQuery(document).ready(function($) {
     $('#universal_local_media_upload_image').click(function(e) {
         e.preventDefault();
         openMediaUploader('image', '#universal_local_image_attachment_ids');
+    });
+
+    // Handle click events for each clear button
+    $('#universal_local_media_clear_all_video').click(function(e) {
+        e.preventDefault();
+        clearAllMedia('video');
+        $(this).fadeOut();
+    });
+
+    $('#universal_local_media_clear_all_audio').click(function(e) {
+        e.preventDefault();
+        clearAllMedia('audio');
+        $(this).fadeOut();
+    });
+
+    $('#universal_local_media_clear_all_image').click(function(e) {
+        e.preventDefault();
+        clearAllMedia('image');
+        $(this).fadeOut();
     });
 });
