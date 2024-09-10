911a96e7c4a6bd646eaf47cf2b6d102cdffde7c3
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2024-09-10 11:25:55 -0400

    Remove unnecessary results attribute from search form template
    - Eliminated `results="0"` attribute from `<form>` and `<input>` tags.
    - Cleaned up the search form template to ensure correct markup.

--- searchform.php
+++ searchform.php
@@ -10,10 +10,10 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 } ?>
 
-<form method="get" id="searchbar" action="<?php echo esc_url( home_url( '/' ) ); ?>" autocomplete="off" results="0">
+<form method="get" id="searchbar" action="<?php echo esc_url( home_url( '/' ) ); ?>" autocomplete="off">
     <label>
         <span class="screen-reader-text"><?php _e( 'Type and hit enter to search', 'tetris' ); ?></span>
-        <input type="search" class="search-field" placeholder="<?php _e( 'Type and hit enter to search', 'tetris' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'tetris' ); ?>" autocomplete="off" results='0' onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;" />
+        <input type="search" class="search-field" placeholder="<?php _e( 'Type and hit enter to search', 'tetris' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'tetris' ); ?>" autocomplete="off" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;">
     </label>
     <?php if ( is_search()) : ?>
         <?php get_template_part('addons/addon-search-filter-control'); ?>
