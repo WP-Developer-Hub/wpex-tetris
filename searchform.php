8e00c587e2b173a63d3213f51a495f5a35c670fb
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2024-12-20 16:13:32 -0500

    Updated search form to remove focus and blur behavior for improved UX

--- searchform.php
+++ searchform.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <form method="get" id="searchbar" action="<?php echo esc_url( home_url( '/' ) ); ?>" autocomplete="off">
     <label>
         <span class="screen-reader-text"><?php _e( 'Type and hit enter to search', 'tetris' ); ?></span>
-        <input type="search" class="search-field" placeholder="<?php _e( 'Type and hit enter to search', 'tetris' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'tetris' ); ?>" autocomplete="off" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;">
+        <input type="search" class="search-field" placeholder="<?php _e( 'Type and hit enter to search', 'tetris' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'tetris' ); ?>" autocomplete="off">
     </label>
     <?php if ( is_search()) : ?>
         <?php get_template_part('addons/addon-search-filter-control'); ?>
