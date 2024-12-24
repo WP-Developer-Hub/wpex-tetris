991bf0e81e7ef096fd9834b2de81427a1b4c17ec
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2024-12-24 12:23:53 -0500

    Refactor post thumbnail display logic in format.php
    - Moved the check for `universal_toggle_post_thumbnail` above the condition checking for post thumbnail and password protection.
    - Ensured the post thumbnail only shows when it's not password-protected, else the placeholder is shown.

--- formats/format.php
+++ formats/format.php
@@ -15,15 +15,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <?php if ( is_singular() ) : ?>
-    <?php if ( has_post_thumbnail() && !post_password_required() ) : ?>
-        <?php if ( get_theme_mod('universal_toggle_post_thumbnail', false) ) : ?>
-            <div id="post-thumbnail" style="background-image: url('<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'thumbnail')); ?>'); background-size: cover; background-position: center center;">
-                <?php the_post_thumbnail( 'wpex-post', array( 'alt' => the_title_attribute( array( 'echo' => false ) ), 'class' => 'u-media-16-9' ) ); ?>
-            </div><!-- /blog-entry-thumbnail -->
-            <?php echo wpx_spacer('', '30'); ?>
+    <?php if ( get_theme_mod('universal_toggle_post_thumbnail', false) ) : ?>
+        <?php if ( has_post_thumbnail() && !post_password_required() ) : ?>
+                <div id="post-thumbnail" style="background-image: url('<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'thumbnail')); ?>'); background-size: cover; background-position: center center;">
+                    <?php the_post_thumbnail( 'wpex-post', array( 'alt' => the_title_attribute( array( 'echo' => false ) ), 'class' => 'u-media-16-9' ) ); ?>
+                </div><!-- /blog-entry-thumbnail -->
+                <?php echo wpx_spacer('', '30'); ?>
+        <?php else : ?>
+            <?php echo wpex_get_post_media_placeholder(); ?>
         <?php endif; ?>
-    <?php else : ?>
-        <?php echo wpex_get_post_media_placeholder(); ?>
     <?php endif; ?>
 <?php else : ?>
     <div class="blog-entry-thumbnail" >
