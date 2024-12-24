7211f7e34f6012cbeadc85923c4f5dd31cd184b6
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2024-12-24 11:15:55 -0500

    Enhance handling of password-protected posts
    - Updated excerpt logic to display a custom message for password-protected posts.
    - Modified thumbnail logic to only display the post thumbnail if the post is not password-protected.
    - Added a media placeholder for password-protected posts or posts without a thumbnail, with 16:9 ratio for singles and 1:1 for others.
    - Implemented a filter to remove the "Protected:" prefix from the titles of password-protected posts.
    
    Changes include updates to content.php, format.php, and helper-functions.php.

--- formats/format.php
+++ formats/format.php
@@ -15,13 +15,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <?php if ( is_singular() ) : ?>
-    <?php if ( has_post_thumbnail() ) : ?>
+    <?php if ( has_post_thumbnail() && !post_password_required() ) : ?>
         <?php if ( get_theme_mod('universal_toggle_post_thumbnail', false) ) : ?>
             <div id="post-thumbnail" style="background-image: url('<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'thumbnail')); ?>'); background-size: cover; background-position: center center;">
                 <?php the_post_thumbnail( 'wpex-post', array( 'alt' => the_title_attribute( array( 'echo' => false ) ), 'class' => 'u-media-16-9' ) ); ?>
             </div><!-- /blog-entry-thumbnail -->
             <?php echo wpx_spacer('', '30'); ?>
         <?php endif; ?>
+    <?php else : ?>
+        <?php echo wpex_get_post_media_placeholder(); ?>
     <?php endif; ?>
 <?php else : ?>
     <div class="blog-entry-thumbnail" >
@@ -32,13 +34,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                     the_post_thumbnail('wpex-entry', array( 'alt' => the_title_attribute( array( 'echo' => false ) ), 'class' => $classes));
                 ?>
             <?php else : ?>
-                <div class="u-media-1-1 u-media-missing-img u-flex u-ai-center u-jc-center" title="<?php the_title(); ?>">
-                    <?php if (post_password_required() ) : ?>
-                        <span class="dashicons dashicons-lock"></span>
-                    <?php else : ?>
-                        <span class="<?php echo universal_get_post_format_icon_classes(get_post_format()); ?>"></span>
-                    <?php endif; ?>
-                </div>
+                <?php echo wpex_get_post_media_placeholder(); ?>
             <?php endif; ?>
             <?php if ( get_theme_mod('universal_toggle_recent_post_badge', 'true')) : echo wpx_recent_post_badge(get_the_ID(), get_theme_mod('universal_recent_post_keep_badge_for', 7)); endif; ?>
         </a>
