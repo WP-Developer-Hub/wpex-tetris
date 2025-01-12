dfb479914ac41e097a1dbbb896c9ad3bb5289017
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2025-01-12 15:48:52 -0500

    Fix: Enhance error message for empty pages and adjust page layout
    - Added a specific error message for pages (`is_page()`) when no content is available. Informs users to check back later or visit the homepage.
    - Updated `page.php` to display an error message when no content is found, using `get_template_part( 'addons/addon-error-message' )`.
    - Improved `.entry .inner-post` layout in CSS to ensure consistent spacing and margins across different page types.

--- addons/addon-error-message.php
+++ addons/addon-error-message.php
@@ -3,7 +3,8 @@
         <?php _e('Oops, We\'re Sorry', 'tetris'); ?>
     </h1>
     <p id="error-message-text" class="u-fs-18 u-flex u-flex-col u-ai-c">
-        <?php if (is_search()) :
+        <?php
+        if (is_search()) :
             _e('Unfortunately, nothing matched your search. Please try different keywords or adjust the post format filter.', 'tetris');
         elseif (is_author()) :
             _e('Unfortunately, there are no posts by this author yet. Please check back later for updates.', 'tetris');
@@ -13,10 +14,11 @@
             _e('Unfortunately, no posts were found for this category. Feel free to explore other categories.', 'tetris');
         elseif (is_tag()) :
             _e('Unfortunately, no posts were found with this tag. Try using a different tag.', 'tetris');
+        elseif (is_page()) :
+            _e('Unfortunately, there is no content available on this page. Please check back later or go to the homepage.', 'tetris');
         else:
             _e('Unfortunately, there are no posts available at the moment. Please check back later.', 'tetris');
-        endif; ?>
+        endif;
+        ?>
     </p>
 </div>
-
-
