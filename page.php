181edbd6c45b8ed0c6001dacc6db526efb8ee939
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2025-07-06 17:43:58 -0400

    Enhance: Add Customizer toggle for page sidebar visibility
    - Added "Page Settings" section to the Customizer with a toggle to enable or disable the sidebar on all pages.
    - Updated page.php to conditionally display the sidebar and related classes based on the Customizer setting.

--- page.php
+++ page.php
@@ -20,17 +20,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header();
 
-if ( have_posts()) : while (have_posts() ) : the_post(); ?>
-    <div id="single-page-content" class="container clearfix">
+$show_sidebar = get_theme_mod('universal_toggle_page_sidebar', true);
+
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div id="single-page-content" class="container clearfix<?php echo ( $show_sidebar ? ' has-sidebar-bg' : '' ); ?>">
         <!-- Page wrapper with post_class() applied -->
-        <article id="page" <?php post_class('entry clearfix'); ?>>
-            <h2 class="screen-reader-text">
+        <article id="page" <?php post_class( 'entry' . ( $show_sidebar ? ' has-sidebar' : '' ) . ' clearfix' ); ?>>
+            <h1 class="screen-reader-text">
                 <?php echo sprintf( __( '%1$s page For %2$s', 'tetris' ), get_the_title(), get_bloginfo('name') ); ?>
             </h2>
             <div id="inner-page" class="u-pos-rel inner-post">
                 <?php the_content(); ?>
             </div><!-- .inner-post -->
-        </article><!-- .entry -->
+        </article><!-- #page -->
+
+        <?php if ( $show_sidebar ) { get_sidebar(); } ?>
+
     </div><!-- #single-page-content -->
 <?php endwhile; else: ?>
     <?php get_template_part( 'addons/addon-error-message' ); ?>
