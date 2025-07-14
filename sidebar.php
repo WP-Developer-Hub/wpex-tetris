f872a82065ccbb38959b5c5b381c2748a90b426c
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2025-07-14 18:56:50 -0400

    Add sidebar settings section and toggles for search box/mobile widgets
    - Add 'Sidebar Settings' section to Customizer under the main theme panel
    - Add toggle to hide search box in sidebar with description
    - Add toggle to hide sidebar widgets on mobile devices (default true) with description
    - Update sidebar.php to conditionally display search box and apply mobile widget hiding based on Customizer settings
    - Use WPX_Toggle_Switch_Control for both toggles to enhance Customizer UI

--- sidebar.php
+++ sidebar.php
@@ -9,12 +9,15 @@
  * @since     1.0.0
  */ ?>
 
-<aside id="sidebar" class="u-wide clearfix">
-    <section class="sidebar-box widget_search clearfix">
-        <h4 class="screen-reader-text"><?php _e('Search', 'tetris'); ?></h4>
-        <?php get_search_form(); ?>
-    </section>
-    <div class="sidebar-widgets clearfix">
+<aside id="sidebar" class="clearfix">
+    <?php if ( ! get_theme_mod( 'universal_hide_sidebar_search', false ) ) : ?>
+        <section class="sidebar-box widget_search clearfix">
+            <h4 class="screen-reader-text"><?php _e('Search', 'tetris'); ?></h4>
+            <?php get_search_form(); ?>
+        </section>
+    <?php endif; ?>
+
+    <div class="<?php echo esc_attr(get_theme_mod('universal_hide_sidebar_mobile', false) ? ' sidebar-widgets' : ''); ?> clearfix">
         <?php dynamic_sidebar( 'sidebar' ); ?>
     </div>
 </aside><!-- #sidebar -->
