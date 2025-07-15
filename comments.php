c0c3f38c45f7a449125a98ee7ff0f6e80d9db2cb
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2025-07-15 18:05:29 -0400

    Refactor: clean up comments template and update callback reference
    - Updated comment callback reference from wpex_comment() to wpex_comments_output()
    - Corrected file path in docblock from functions/comments-callback.php to inc/comments.php
    - Wrapped return condition in braces for consistency
    - Removed starter placeholder comment ("You can start editing here")
    - Combined pagination markup into single PHP blocks for better readability
    - Cleaned up comment output structure without changing logic

--- comments.php
+++ comments.php
@@ -3,8 +3,8 @@
  * The template for displaying Comments.
  *
  * The area of the page that contains both current comments and the comment
- * form. The actual display of comments is handled by a callback to
- * wpex_comment() which is located at functions/comments-callback.php
+ * The actual display of comments is handled by a callback to
+ * wpex_comments_output() which is located at inc/comments.php
  *
  * @package   Testris WordPress Theme
  * @author    Alexander Clarke
@@ -17,28 +17,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /* If a post password is required or no comments are given and comments/pings are closed, return. */
-if ( post_password_required() || ( ! have_comments() && ! comments_open() && !pings_open() ) )
+if ( post_password_required() || ( ! have_comments() && ! comments_open() && !pings_open() ) ) {
 	return;
+}
 ?>
 <div id="commentsbox" class="boxframe">
 	<div id="comments" class="comments-area clearfix">
-		<?php // You can start editing here -- including this comment! ?>
 		<?php if ( have_comments() ) : ?>
 			<h3 class="comments-title widget-title"><span><?php comments_popup_link(__('Leave a comment', 'tetris'), __('1 Comment', 'tetris'), __('% Comments', 'tetris'), 'comments-link', __('Comments closed', 'tetris')); ?></span></h3>
-			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
-            <?php echo universal_custom_paginate_comments_links('above'); ?>
-			<!-- /coment-nav-above -->
-			<?php endif; ?>
+			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
+                /* coment-nav-above */
+                echo universal_custom_paginate_comments_links('above');
+			endif; ?>
 			<ol id="commentlist" class="commentlist">
 				<?php wp_list_comments( array( 'callback' => 'wpex_comments_output' ) ); ?>
 			</ol><!-- /commentlist -->
-			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
-            <?php echo universal_custom_paginate_comments_links('below'); ?>
-            <!-- /coment-nav-below -->
-			<?php endif; ?>
-		<?php endif; ?>
-		<?php if (!comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-			<?php /* <p class="nocomments"><?php _e( 'Comments are closed.', 'tetris' ); ?></p> */ ?>
+			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
+                /* coment-nav-below */
+                echo universal_custom_paginate_comments_links('below');
+			endif; ?>
 		<?php endif; ?>
 		<?php comment_form( array(
 			'title_reply' => '<span class="widget-title">'. __( 'Leave a Reply', 'tetris' ) .'</span>'
