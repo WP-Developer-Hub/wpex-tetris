df26854015184ae3e5a34748dd78dfc0fedde84d
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2025-10-08 09:49:51 -0400

    fix: prevent mediaelement script loading in Customizer preview
    Add a condition to wpex_enqueue_mediaelement_fix() to skip enqueue when
    in the Customizer preview mode (is_customize_preview()). This avoids
    conflicts or redundant script loading during theme customization.

--- inc/scripts.php
+++ inc/scripts.php
@@ -56,7 +56,7 @@ if ( ! function_exists( 'wpex_load_scripts' ) ) {
  
 if ( ! function_exists( 'wpex_enqueue_mediaelement_fix' ) ) {
     function wpex_enqueue_mediaelement_fix() {
-        if ( wp_script_is( 'mediaelement', 'enqueued' ) ) {
+        if ( wp_script_is( 'mediaelement', 'enqueued' ) && !is_customize_preview() ) {
             $dir = get_template_directory_uri();
 
             // Enqueue Universal player stylesheet
