a02d788cd70dce1f8856bcdcd38c605d56366c36
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2026-05-18 13:55:20 -0400

    perf: remove legacy media element resize and wrapper scripts
    - Drop manual jQuery DOM-wrapping loop for media elements
    - Remove redundant JS fullscreen event listeners and style injection
    - Retain core MediaElementPlayer prototype class assignment
    - Remove !is_customize_preview() constraint in inc/scripts.php

--- js/wp-mediaelement-fix.js
+++ js/wp-mediaelement-fix.js
@@ -6,30 +6,4 @@ jQuery(document).ready(function($) {
     MediaElementPlayer.prototype.buildexampleclass = function(player) {
         player.container.addClass('universal-mejs-container u-media-16-9');
     };
-
-    function handleFullscreenChange() {
-        if (document.fullscreenElement) {
-            $('video').css('max-height', 'none');
-        } else {
-            $('video').css('max-height', '512px');
-        }
-    }
-
-    $(document).on( 'fullscreenchange webkitfullscreenchange mozfullscreenchange msfullscreenchange', handleFullscreenChange );
-
-    handleFullscreenChange();
-
-    function wrapMediaElements() {
-        $('.wp-video, .wp-playlist').each(function() {
-            if (!$(this).closest('#post-media').length &&
-                !$(this).closest('.post-media').length) {
-                if (!$(this).hasClass('wp-audio-playlist')) {
-                    var $wrapper = $('<div class="post-media"></div>');
-                    $(this).wrap($wrapper);
-                }
-                $(this).css('width', '100%');
-            }
-        });
-    }
-    wrapMediaElements();
 });
